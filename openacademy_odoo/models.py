f464b1bb27e51d26d25f7d6e8ab4ac17c0ceafd1
root <helton.penariol@outtech.com.br>
2018-08-24 16:03:14 -0300

    alteração

--- openacademy_odoo/models.py
+++ openacademy_odoo/models.py
@@ -6,10 +6,10 @@ from odoo import models, fields, api, exceptions, _
 class Course(models.Model):
     _name = 'openacademy.course'
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(string="Titulo", required=True)
     description = fields.Text()
 
-    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsavel", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
     @api.multi
